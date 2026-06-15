d65d3f05b0b739497944dbf710278d769ba1ada2
AartiRai <143518187+AartRai@users.noreply.github.com>
2026-06-15 21:59:00 +0530

    Add MedianFinder class for finding median
    Implemented MedianFinder class to find median from data stream using two heaps.

---
+++ Heap/Questions/Find Median from Data Stream.cpp
@@ -0,0 +1,31 @@
+//https://leetcode.com/problems/find-median-from-data-stream/description/?envType=problem-list-v2&envId=heap-priority-queue
+
+class MedianFinder {
+public:
+    priority_queue<int> maxHeap;
+    priority_queue<int,vector<int>,greater<int>> minHeap;
+
+    MedianFinder() {
+    }
+    
+    void addNum(int num) {
+        maxHeap.push(num);
+        minHeap.push(maxHeap.top());
+        maxHeap.pop();
+
+        if(minHeap.size() > maxHeap.size()){
+            maxHeap.push(minHeap.top());
+            minHeap.pop();
+        }
+    }
+    
+    double findMedian() {
+        if(maxHeap.size() == minHeap.size()){
+            return (maxHeap.top() + minHeap.top())/2.0;
+        }else{
+            return maxHeap.top();
+        }
+    }
+};
+//TC: addNum → O(log n), findMedian → O(1)
+//SC: O(n)
