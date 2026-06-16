477655e1317042347e4c66d195bfd0fd22c9994b
AartiRai <143518187+AartRai@users.noreply.github.com>
2026-06-16 18:08:28 +0530

    topKFrequent function

---
+++ Heap/Top K Frequent Elements.cpp
@@ -0,0 +1,24 @@
+//https://leetcode.com/problems/top-k-frequent-elements/description/?envType=problem-list-v2&envId=heap-priority-queue
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        map<int,int>mp;
+        for(int x:nums){
+            mp[x]++;
+        }
+
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>pq;
+        for(auto it:mp){
+            pq.push({it.second,it.first});
+        }
+
+        while(pq.size()>k){
+            pq.pop();
+        }
+        vector<
+    }
+};
+
+//TC = O(n log m + m log k)
+//SC = O(m + k)
